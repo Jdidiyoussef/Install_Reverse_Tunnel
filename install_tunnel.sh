de7c9de1e8830480f7bd40375095d492754f35f3
DIJIYOUSS <youssef.el.jdidi@gmail.com>
2026-06-10 23:20:45 +0100

    corrige

--- install_tunnel.sh
+++ install_tunnel.sh
@@ -49,21 +49,21 @@ elif [ "$MODE" == "Pi" ]; then
 	echo "=== Création du service systemd ==="
 
 	sudo tee /etc/systemd/system/reverse-tunnel_test.service > /dev/null <<EOF
-[Unit]
-Description=Reverse SSH Tunnel to Contabo
-After=network-online.target
-Wants=network-online.target
+	[Unit]
+	Description=Reverse SSH Tunnel to Contabo
+	After=network-online.target
+	Wants=network-online.target
 
-[Service]
-User=${USER_PI}
-Environment="AUTOSSH_GATETIME=0"
-ExecStartPre=/bin/sleep 10
-ExecStart=/usr/bin/autossh -N -o "ServerAliveInterval 30" -o "ServerAliveCountMax 3" -R ${REMOTE_PORT}:localhost:22 root@${SERVER_IP}
-Restart=always
-RestartSec=5
+	[Service]
+	User=${USER_PI}
+	Environment="AUTOSSH_GATETIME=0"
+	ExecStartPre=/bin/sleep 10
+	ExecStart=/usr/bin/autossh -N -o "ServerAliveInterval 30" -o "ServerAliveCountMax 3" -R ${REMOTE_PORT}:localhost:22 root@${SERVER_IP}
+	Restart=always
+	RestartSec=5
 
-[Install]
-WantedBy=multi-user.target
+	[Install]
+	WantedBy=multi-user.target
 	EOF
 
 	echo "=== Génération de clé SSH ==="
